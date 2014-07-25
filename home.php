0953f6e213843e74120a60921e33db034add18fe
Michael Caldera <michael@Michaels-MacBook-Pro.local>
2014-07-25 01:59:50 +0100

    Adding pages

---
+++ home.php
@@ -0,0 +1,34 @@
+<div class="jumbotron">
+    <div class="row">
+    <div class="col-xs-5 col-md-2">
+      <a href="#" class="thumbnail">
+          <img src="pub/img/michael.jpg" />
+      </a>
+    </div>
+    <h1>Michael Caldera</h1>
+    <p>
+        A London based Software Engineer interested in cloud backend development.
+    </p>
+    <p><a class="btn btn-primary btn-lg" role="button">Learn more</a></p>
+  </div>
+</div>
+
+<blockquote>
+  <p>When you feel the need to write a comment, first try to refactor the code so that any comment becomes superflouus. </p>
+  <footer><cite title="Source Title">Martin Fowler</cite>, Refactoring: Improving the Design of Existing Code by Martin Fowler, Kent Beck (Contributor), John Brant (Contributor), William Opdyke, don Roberts , ISBN: 0201485672</footer>
+</blockquote>
+
+
+<blockquote class="blockquote-reverse">
+  <p>When you feel the need to write a comment, first try to refactor the code so that any comment becomes superflouus. </p>
+  <footer><cite title="Source Title">Martin Fowler</cite>, Refactoring: Improving the Design of Existing Code by Martin Fowler, Kent Beck (Contributor), John Brant (Contributor), William Opdyke, don Roberts , ISBN: 0201485672</footer>
+</blockquote>
+
+<span class="label label-default">Default</span>
+<span class="label label-primary">Primary</span>
+<span class="label label-success">Success</span>
+<span class="label label-info">Info</span>
+<span class="label label-warning">Warning</span>
+<span class="label label-danger">Danger</span>
+
+<p>Last Update: Sep 25th, 2013</p>
